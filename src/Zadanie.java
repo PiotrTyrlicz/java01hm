d82454317dbb42127d4237f1f39e54dd26d3a439
Piotr Tyrlicz <tyrlicz@wp.pl>
2017-09-13 11:35:30 +0200

    "1 czesc polecenia"

--- src/Zadanie.java
+++ src/Zadanie.java
@@ -1,6 +1,8 @@
 public class Zadanie
 {
     public static void main(String[] args) {
-        System.out.print("Hello");
+        System.out.println("Uczę");
+        System.out.println("się");
+        System.out.println("programowania");
     }
 }
